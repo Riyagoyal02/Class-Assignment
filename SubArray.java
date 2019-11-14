cabb7844070edeb9e634e5eaa3d4e0329ed4e607
Riya Goyal <44223849+Riyagoyal02@users.noreply.github.com>
2019-11-14 11:47:51 +0530

    Update SubArray.java

--- SubArray.java
+++ SubArray.java
@@ -6,15 +6,14 @@ public class SubArray {
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		int k=sc.nextInt();
+		
 		System.out.println("enter size");
 		int n=sc.nextInt();
 		int arr[]=new int[n];
 		for(int i=0;i<n;i++)
 			arr[i]=sc.nextInt();
-			int count=0;
-			n=n-k+1;
-			count=n*(n+1)/2;
+		int k=sc.nextInt();
+			int count=(int)((n-k+1)*(n-k+2)/(2.0));
 			System.out.println(count);
 			
 		
